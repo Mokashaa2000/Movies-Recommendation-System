e02cf2d2fadb36f13758adab63c10f05c48e63b8
Mohammed Kashalo <55239590+Mokashaa2000@users.noreply.github.com>
2023-05-27 04:03:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 #import numpy as np
 import streamlit as st
 import joblib
+from scipy.sparse import csr_matrix
 pd.set_option("display.max_columns",None)
 pd.set_option("display.max_rows",150)
 pd.set_option("expand_frame_repr", False)
