3c1da6f704a40d567dc7ba722fd3023cdf9e0540
Mohammed Kashalo <55239590+Mokashaa2000@users.noreply.github.com>
2023-06-01 20:55:38 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.markdown(
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-image: url("https://wallpapercave.com/w/wp1945897");
              background-attachment: fixed;
              background-size: cover
          }}
